e40d10bbe2c4912600c057f015c77bd901c1e4a3
cambyze <cambyze@gmail.com>
2020-11-25 02:37:15 +0100

    Reorganization of the project by servers, services and client-ui Use of eureka for application instantiation
    Signed-off-by: cambyze <cambyze@gmail.com>

---
+++ services/products/src/main/java/com/cambyze/training/springboot/microservice/h2/grocery/products/GroceryProductApplication.java
@@ -0,0 +1,28 @@
+package com.cambyze.training.springboot.microservice.h2.grocery.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Spring boot application for products management
+ * <p>
+ * with API REST documentation by Swagger
+ * 
+ * @author Thierry Nestelhut
+ * @see <a href="http://localhost:9090/swagger-ui/">API documentation</a>
+ * @see <a href="https://github.com/cambyze">cambyze GitHub</a>
+ */
+@SpringBootApplication(
+    scanBasePackages = {"com.cambyze.training.springboot.microservice.h2.grocery.products",
+        "com.cambyze.commons.microservices.web.controller.impl"})
+@EnableConfigurationProperties
+@EnableDiscoveryClient
+public class GroceryProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GroceryProductApplication.class, args);
+  }
+
+}
