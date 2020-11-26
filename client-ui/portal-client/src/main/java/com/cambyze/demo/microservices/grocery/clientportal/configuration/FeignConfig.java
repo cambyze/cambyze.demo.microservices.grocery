210e64063c58f3da096cbac7a7bc12367a8c5c2c
cambyze <cambyze@gmail.com>
2020-11-26 17:42:11 +0100

    Add spring security for the gateway server
    Signed-off-by: cambyze <cambyze@gmail.com>

--- client-ui/portal-client/src/main/java/com/cambyze/demo/microservices/grocery/clientportal/configuration/FeignConfig.java
+++ client-ui/portal-client/src/main/java/com/cambyze/demo/microservices/grocery/clientportal/configuration/FeignConfig.java
@@ -3,13 +3,20 @@ package com.cambyze.demo.microservices.grocery.clientportal.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.cambyze.demo.microservices.grocery.clientportal.exceptions.MicroservicesExceptionsController;
+import feign.auth.BasicAuthRequestInterceptor;
 
 @Configuration
-public class FeignExceptionConfig {
+public class FeignConfig {
 
   @Bean
   public MicroservicesExceptionsController microservicesExceptionsController() {
     return new MicroservicesExceptionsController();
-  };
+  }
+
+  @Bean
+  public BasicAuthRequestInterceptor changeBasicAuthRequest() {
+    return new BasicAuthRequestInterceptor("cambyze-api-full-access",
+        "e120b07e7734ed541d87d1846a23428b");
+  }
 
 }
