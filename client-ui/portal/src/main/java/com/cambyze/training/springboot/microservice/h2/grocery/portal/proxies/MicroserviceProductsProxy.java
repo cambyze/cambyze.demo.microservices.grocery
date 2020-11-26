2093d1096e3cf37ad0faf7651d80dd325f709b32
cambyze <cambyze@gmail.com>
2020-11-26 10:35:20 +0100

    load balancing with Ribbon
    Signed-off-by: cambyze <cambyze@gmail.com>

--- client-ui/portal/src/main/java/com/cambyze/training/springboot/microservice/h2/grocery/portal/proxies/MicroserviceProductsProxy.java
+++ client-ui/portal/src/main/java/com/cambyze/training/springboot/microservice/h2/grocery/portal/proxies/MicroserviceProductsProxy.java
@@ -1,6 +1,7 @@
 package com.cambyze.training.springboot.microservice.h2.grocery.portal.proxies;
 
 import java.util.List;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,8 @@ import com.cambyze.training.springboot.microservice.h2.grocery.portal.beans.Prod
  * @author Thierry Nestelhut
  * @see <a href="https://github.com/cambyze">cambyze GitHub</a>
  */
-@FeignClient(name = "microservice-products", url = "http://localhost:9090")
+@FeignClient(name = "training-products")
+@RibbonClient(name = "training-products")
 public interface MicroserviceProductsProxy {
 
   public static final String PATH_PRODUCT = "/products";
